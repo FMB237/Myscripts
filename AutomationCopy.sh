71db92a82cd90bf6f72869155c33070eeb8366c3
FMB237 <miguelfouenanf@gmail.com>
2025-11-12 23:56:18 +0100

    Third Commit

---
+++ AutomationCopy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -O http://192.168.122.120:8080/Install-libvirt-daemaon.sh
+curl -O http://192.168.122.120:8080/DisableUnattendedUpgrades.sh
+ curl -O http://192.168.122.120:8080/EnablesUnattendedUpgrades.sh
+curl -O http://192.168.122.120:8080/GenerateSshKey.sh
